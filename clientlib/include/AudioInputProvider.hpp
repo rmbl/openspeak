6d2b5c4fdaf5c685f77ef57f43120dd03a1bc5e7
Philipp Gildein <rmbl@openspeak-project.org>
2009-09-07 21:27:14 +0200

    Added AudioInput class

--- clientlib/include/AudioInputProvider.hpp
+++ clientlib/include/AudioInputProvider.hpp
@@ -15,55 +15,78 @@
   *  with this program; if not, write to the Free Software Foundation, Inc.,
   *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
   */
-  
+
 #ifndef __OS_CLIENT_AUDIO_INPUT_PROVIDER_HPP__
 #define __OS_CLIENT_AUDIO_INPUT_PROVIDER_HPP__
 
 #include "PluginInterfaceProvider.hpp"
+#include "PluginInterface.hpp"
 
 namespace openSpeak
 {
-    
+
     namespace Client
     {
-        
+
+        /** \class AudioInput
+         *  \brief Abstract class to implement an AudioInput interface
+         */
+        class AudioInput : public PluginInterface
+        {
+         public:
+            /** \brief The virtual deconstructor of the AudioInput class */
+            virtual ~AudioInput (void) { }
+
+            /** \brief Gets the audio input from the implementation
+             *  \return The audio input frame
+             */
+            virtual char* getAudioInput (void) = 0;
+        };
+
+        /** \class AudioInputProvider
+         *  \brief Provides a public interface to one AudioInput class
+         *
+         *  The AudioInputProvider class provides a public interface for
+         *  functions of the AudioInput class.
+         */
         class AudioInputProvider : public PluginInterfaceProvider
         {
          public:
             /** \brief The constructor of the AudioInputProvider class */
             AudioInputProvider (void);
-          
+
             /** \brief The deconstructor of the AudioInputProvider class */
             virtual ~AudioInputProvider (void) { }
-                
+
             /** \brief Use the specified interface for all operations
              *  \param name The name of the interface to use
              *  \throw Throws an exception if the interface is unknown
              */
             virtual void useInterface (const std::string &name);
-                
+
             /** \brief Use a "default" interface
              *  \throw Throws an exception if no interface is available
              *          or the interface fails to load.
-             * 
+             *
              *  Which interface to use is in the responsibility of the
              *  subclass developers. Possiblities include using the first one
              *  in the vector or including a list of favorites.
              */
             virtual void useDefaultInterface (void);
-            
+
             /** \brief Get the input from the choosen interface
+             *  \return The audio input frame
              *  \throw Throws an exception if no interface got chosen
              */
             char* getAudioInput (void) const;
-            
+
          protected:
             AudioInput *mDefaultInput;
-            
+
         };
-        
+
     }
-    
+
 }
 
 #endif
